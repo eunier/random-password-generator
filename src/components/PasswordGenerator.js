550920c5ea57b7e6c5b750c335d6fdd12d44d7c5
Yunier Alvarez <yunieralvarez@librem.one>
2020-10-10 23:16:16 -0400

    clean up

--- src/components/PasswordGenerator.js
+++ src/components/PasswordGenerator.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
+import { v4 as uuid } from 'uuid';
 
 import useRandomPasswordGenerator from './hooks/useRandomPasswordGenerator';
 
 const PasswordGenerator = () => {
   const [withLowercase, setWithLowercase] = useState(false);
+  const [withUppercase, setWithUppercase] = useState(false);
   const [randomPassword, generateRandomPassword] = useRandomPasswordGenerator();
-
-  // const useCallback
 
   useEffect(() => generateRandomPassword(), [generateRandomPassword]);
 
@@ -14,31 +14,47 @@ const PasswordGenerator = () => {
     e.preventDefault();
   };
 
+  const checkboxCtrl = [
+    [uuid(), withLowercase, setWithLowercase, 'Lowercase Letters'],
+    [uuid(), withUppercase, setWithUppercase, 'Uppercase Letters'],
+  ].map(([key, state, setState, checkboxDesc]) => ({
+    key,
+    state,
+    setState,
+    checkboxDesc,
+  }));
+
   return (
     <div className="container">
       <form onSubmit={onGenerateRandomPasswordHandler}>
-        <div className="row">
-          <div className="col d-flex justify-content-center">
-            <div className="form-group">
-              <input
-                className="form-check-input"
-                id="lowercase-letters"
-                type="checkbox"
-                checked={withLowercase}
-                onChange={e => setWithLowercase(e.target.checked)}
-              />
-              <label className="form-check-label" htmlFor="lowercase-letters">
-                Lowercase Letters
-              </label>
+        {checkboxCtrl.map(({ key, state, setState, checkboxDesc }) => (
+          <div className="row" key={key}>
+            <div className="col d-flex justify-content-center">
+              <div className="form-group">
+                <input
+                  className="form-check-input"
+                  id={`lowercase-letters-${key}`}
+                  type="checkbox"
+                  checked={state}
+                  onChange={e => setState(e.target.checked)}
+                />
+                <label
+                  className="form-check-label"
+                  htmlFor={`lowercase-letters-${key}`}
+                >
+                  {checkboxDesc}
+                </label>
+              </div>
             </div>
           </div>
-        </div>
+        ))}
 
         <div className="row">
           <div className="col d-flex justify-content-center">
             <button
               className="btn btn-primary"
               type="submit"
+              disabled={true}
               onClick={() => generateRandomPassword()}
             >
               Generate Random Password
--- src/components/hooks/useRandomPasswordGenerator.js
+++ src/components/hooks/useRandomPasswordGenerator.js
@@ -3,11 +3,13 @@ const { useState, useCallback } = require('react');
 const useRandomPasswordGenerator = () => {
   const [password, setPassword] = useState(null);
 
-  const generateRandomPassword = useCallback(() => {
-    return setPassword(Math.floor(Math.random() * 9) + 1);
-  }, []);
+  const generateRandomPassword = useCallback(
+    () => setPassword(Math.floor(Math.random() * 9) + 1),
+    []
+  );
 
   return [password, generateRandomPassword];
 };
 
 export default useRandomPasswordGenerator;
+ 
