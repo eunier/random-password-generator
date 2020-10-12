26b24b600d420e9fa8124b9b5974d8b6554ebeeb
Yunier Alvarez <yunieralvarez@librem.one>
2020-10-11 20:37:07 -0400

    clean up

--- src/components/About.js
+++ src/components/About.js
@@ -4,10 +4,15 @@ const About = () => (
   <div className="row">
     <div className="col d-flex justify-content-center">
       <p>
-        Made by Yunier Alvarez. Source code at:{" "}
-        <a href="https://github.com/eunier/random-password-generator">
-          GibHub
+        Made by Yunier Alvarez. Source code
+        <a
+          href="https://github.com/eunier/random-password-generator"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          here
         </a>
+        .
       </p>
     </div>
   </div>
